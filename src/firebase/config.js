a6e130ab0ce139945ea586a7d5607c7ed65e2608
= <huedlup@gmail.com>
2022-03-30 11:55:57 -0700

    Actividad_10 primer version

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4KaJ-MArSxzaByKuzyaM7QU1rBTRwt78',
-  authDomain: 'actividad-10-utch.firebaseapp.com',
-  databaseURL: 'https://actividad-10-utch-default-rtdb.firebaseio.com',
-  projectId: 'actividad-10-utch',
-  storageBucket: 'actividad-10-utch.appspot.com',
-  messagingSenderId: '912457574451',
-  appId: '1:912457574451:web:27ad1971316a81d0595b46',
+  apiKey: "AIzaSyBckiCpbLKgPcXQZXy3LANjUuwHeH51mkU",
+  authDomain: "actividad-10-b5049.firebaseapp.com",
+  databaseURL: "https://actividad-10-b5049-default-rtdb.firebaseio.com",
+  projectId: "actividad-10-b5049",
+  storageBucket: "actividad-10-b5049.appspot.com",
+  messagingSenderId: "45137169149",
+  appId: "1:45137169149:web:28fec928349d102f9f0c94"
 };
 
 if (!firebase.apps.length) {
